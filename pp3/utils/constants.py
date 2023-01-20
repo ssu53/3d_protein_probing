04b24591e3f02b92daa1f5ebd96c0c8b9000dd37
Kyle Swanson <swansonk.14@gmail.com>
2023-01-20 14:15:34 -0800

    Add AA 3 to 1 dict

--- pp3/utils/constants.py
+++ pp3/utils/constants.py
@@ -1,1 +1,8 @@
 """Constant values used across multiple modules."""
+# Mapping from amino acid three-letter codes to one-letter codes
+AA_3_TO_1 = {
+    'CYS': 'C', 'ASP': 'D', 'SER': 'S', 'GLN': 'Q', 'LYS': 'K',
+    'ILE': 'I', 'PRO': 'P', 'THR': 'T', 'PHE': 'F', 'ASN': 'N',
+    'GLY': 'G', 'HIS': 'H', 'LEU': 'L', 'ARG': 'R', 'TRP': 'W',
+    'ALA': 'A', 'VAL': 'V', 'GLU': 'E', 'TYR': 'Y', 'MET': 'M'
+}
