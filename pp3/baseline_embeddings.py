Reducing weight decay from 1.0 to 0.1 and fixing baseline normalization to avoid divide by zero and avoid normalization of one-hot

pp3/baseline_embeddings.py
@@ -2,7 +2,7 @@
 import torch
 from Bio.SeqUtils.ProtParam import ProteinAnalysis
 
-from pp3.utils.constants import AA_1
+from pp3.utils.constants import AA_1, AA_1_TO_INDEX
 from pp3.utils.constants import BLOSUM62_AA_TO_VECTOR
 
 
@@ -28,18 +28,26 @@ def get_baseline_protein_embedding(sequence: str) -> torch.Tensor:
     # Combine features to create the protein embedding
     protein_embedding = torch.FloatTensor([
         len(sequence),
-        *aa_frequencies,
+        *aa_frequencies,  # length 22
         protein_analysis.molecular_weight(),
         protein_analysis.aromaticity(),
         protein_analysis.instability_index(),
         protein_analysis.gravy(),
         protein_analysis.isoelectric_point(),
         protein_analysis.charge_at_pH(7.4),
-        *protein_analysis.secondary_structure_fraction(),
-        *protein_analysis.molar_extinction_coefficient()
+        *protein_analysis.secondary_structure_fraction(),  # length 3
+        *protein_analysis.molar_extinction_coefficient()  # length 2
     ])
 
     return protein_embedding
+
+
+def get_baseline_protein_one_hot_mask() -> torch.Tensor:
+    """Get a binary mask indicating which features in the protein baseline embedding are one-hot encoded."""
+    length = get_baseline_residue_embedding_index(sequence='A', index=0).shape[0]
+    one_hot_mask = torch.zeros(length, dtype=torch.bool)
+
+    return one_hot_mask
 
 
 def get_baseline_residue_embedding_index(sequence: str, index: int) -> torch.Tensor:
@@ -57,7 +65,7 @@ def get_baseline_residue_embedding_index(sequence: str, index: int) -> torch.Ten
     """
     # Create a one-hot vector for the residue
     residue_one_hot = [0] * len(AA_1)
-    residue_one_hot[AA_1.index(sequence[index])] = 1
+    residue_one_hot[AA_1_TO_INDEX[sequence[index]]] = 1
 
     # Compute the residue's relative position in the protein sequence
     residue_position = index / len(sequence)
@@ -67,10 +75,10 @@ def get_baseline_residue_embedding_index(sequence: str, index: int) -> torch.Ten
 
     # Combine features to create the residue embedding
     residue_embedding = torch.FloatTensor([
-        *residue_one_hot,
+        *residue_one_hot,  # length 22
         residue_position,
         protein_length,
-        *BLOSUM62_AA_TO_VECTOR[sequence[index]]
+        *BLOSUM62_AA_TO_VECTOR[sequence[index]]  # length 24
     ])
 
     return residue_embedding
@@ -89,3 +97,12 @@ def get_baseline_residue_embedding(sequence: str) -> torch.Tensor:
     ])
 
     return residue_embeddings
+
+
+def get_baseline_residue_one_hot_mask() -> torch.Tensor:
+    """Get a binary mask indicating which features in the residue baseline embedding are one-hot encoded."""
+    length = get_baseline_residue_embedding_index(sequence='A', index=0).shape[0]
+    one_hot_mask = torch.zeros(length, dtype=torch.bool)
+    one_hot_mask[:len(AA_1)] = 1
+
+    return one_hot_mask

pp3/data.py
@@ -8,7 +8,12 @@ import torch
 from sklearn.model_selection import train_test_split
 from torch.utils.data import DataLoader, Dataset
 
-from pp3.baseline_embeddings import get_baseline_protein_embedding, get_baseline_residue_embedding
+from pp3.baseline_embeddings import (
+    get_baseline_protein_embedding,
+    get_baseline_protein_one_hot_mask,
+    get_baseline_residue_embedding,
+    get_baseline_residue_one_hot_mask
+)
 from pp3.concepts import get_concept_level, get_concept_type
 
 
@@ -253,8 +258,10 @@ class ProteinConceptDataModule(pl.LightningDataModule):
             # Get appropriate baseline embedding method for concept level
             if self.concept_level == 'protein':
                 baseline_embedding_fn = get_baseline_protein_embedding
+                one_hot_mask = get_baseline_protein_one_hot_mask()
             elif self.concept_level.startswith('residue'):
                 baseline_embedding_fn = get_baseline_residue_embedding
+                one_hot_mask = get_baseline_residue_one_hot_mask()
             else:
                 raise ValueError(f'Invalid concept level: {self.concept_level}')
 
@@ -264,15 +271,22 @@ class ProteinConceptDataModule(pl.LightningDataModule):
                 for pdb_id, protein in pdb_id_to_proteins.items()
             }
 
-            # Normalize baseline embeddings using mean/std fit on train embeddings
+            # Get tensor with all train embeddings
             scaler_embeddings = torch.stack([
                 pdb_id_to_embeddings[pdb_id]
                 for pdb_id in self.train_pdb_ids
             ])  # (num_train, embedding_dim)
 
+            # Compute mean and std of train embeddings
             mean = scaler_embeddings.mean(dim=0, keepdim=True)  # (1, embedding_dim)
             std = scaler_embeddings.std(dim=0, keepdim=True, unbiased=False)  # (1, embedding_dim)
+            std[std == 0] = 1  # Avoid dividing by zero
 
+            # Set mean and std of one-hot embeddings to zero and one to avoid normalizing them
+            mean[:, one_hot_mask] = 0
+            std[:, one_hot_mask] = 1
+
+            # Normalize baseline embeddings that are not one-hot using mean/std fit on train embeddings
             pdb_id_to_embeddings = {
                 pdb_id: (embedding - mean) / std
                 for pdb_id, embedding in pdb_id_to_embeddings.items()