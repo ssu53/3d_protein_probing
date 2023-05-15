34c813ff17b2c50722cc72c910158e8b91d89458
Kyle Swanson <swansonk.14@gmail.com>
2023-05-15 11:40:28 -0700

    Results plotting for multiple concepts on same plot

--- scripts/plot_wand_results.py
+++ scripts/plot_wand_results.py
@@ -1,14 +1,17 @@
 """Plot results using W&B output CSV file."""
 from collections import defaultdict
 from pathlib import Path
+from typing import Any
 
 import matplotlib.patches as mpatches
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+from tqdm import tqdm
 
 
-EMBEDDING_METHODS_ORDER = ['baseline', 'plm']
+EMBEDDING_METHODS = ['baseline', 'plm']
+ENCODER_TYPES = ['mlp', 'egnn', 'tfn']
 EMBEDDING_METHOD_TO_HATCH = {
     'baseline': '',
     'plm': '/'
@@ -17,7 +20,6 @@ EMBEDDING_METHOD_TO_UPPER = {
     'baseline': 'Baseline',
     'plm': 'PLM'
 }
-ENCODER_TYPES_ORDER = ['mlp', 'egnn', 'tfn']
 ENCODER_TYPE_TO_COLOR = {
     'mlp': 'tab:blue',
     'egnn': 'tab:orange',
@@ -26,71 +28,114 @@ ENCODER_TYPE_TO_COLOR = {
 OFFSET = 1
 
 
+def default_dict_to_regular(obj: Any) -> dict:
+    if isinstance(obj, defaultdict):
+        obj = {key: default_dict_to_regular(value) for key, value in obj.items()}
+
+    return obj
+
+
 def plot_wand_results(
-        data_path: Path,
-        concept: str,
-        metric: str,
+        data_paths: list[Path],
+        metrics: list[str],
         save_path: Path
 ) -> None:
     """Plot results using W&B output CSV file."""
-    # Load data
-    data = pd.read_csv(data_path)
+    # Check input sizes
+    assert len(data_paths) == len(metrics)
 
-    # Extract run information
-    runs = data.columns[1::3]
-    run_names = [run.split('-')[0].strip().lstrip(f'{concept}_') for run in runs]
-    embedding_methods = [run_name.split('_')[0] for run_name in run_names]
-    encoder_types = [run_name.split('_')[1] for run_name in run_names]
+    # Extract results
+    concept_to_embedding_to_encoder_to_results = defaultdict(lambda: defaultdict(lambda: defaultdict(list)))
+    concept_to_metric = {}
 
-    # Merge results across splits
-    experiment_to_results = defaultdict(list)
-    for run, embedding_method, encoder_type in zip(runs, embedding_methods, encoder_types):
-        experiment_to_results[f'{embedding_method} {encoder_type}'].append(data[run].dropna().iloc[0])
+    for data_path, metric in zip(data_paths, metrics):
+        # Load data
+        data = pd.read_csv(data_path)
 
-    # Set up encoder type order
-    assert set(encoder_types) <= set(ENCODER_TYPES_ORDER)
-    encoder_types_order = [encoder_type for encoder_type in ENCODER_TYPES_ORDER if encoder_type in encoder_types]
+        # Extract run information
+        runs = data.columns[1::3]
+        concepts, run_names, embedding_methods, encoder_types = [], [], [], []
 
-    # Set up embedding method order
-    assert set(embedding_methods) <= set(EMBEDDING_METHODS_ORDER)
-    embedding_methods_order = [embedding_method for embedding_method in EMBEDDING_METHODS_ORDER if embedding_method in embedding_methods]
+        for run in runs:
+            # Get concept
+            concept = None
+            for embedding_method in EMBEDDING_METHODS:
+                if embedding_method in run:
+                    concept = run[:run.index(embedding_method)].strip('_')
+                    break
 
-    # Plot results
-    save_path.parent.mkdir(parents=True, exist_ok=True)
+            if concept is None:
+                raise ValueError(f'Could not find concept in {run}')
 
-    fig, ax = plt.subplots(figsize=(8, 6))
+            # Get run information
+            run_name = run.split('-')[0].strip().replace(f'{concept}_', '')
+            run_names.append(run_name)
 
-    xticks, xticklabels, legend_items = [], [], []
-    for embedding_method_idx, embedding_method in enumerate(embedding_methods_order):
-        legend_items.append(
-            mpatches.Patch(
-                facecolor='lightgray',
-                hatch=EMBEDDING_METHOD_TO_HATCH[embedding_method],
-                label=EMBEDDING_METHOD_TO_UPPER[embedding_method]
-            )
+            concept = concept.replace('_', ' ').title()
+            concepts.append(concept)
+
+            embedding_methods.append(run_name.split('_')[0])
+            encoder_types.append(run_name.split('_')[1])
+
+            concept_to_metric[concept] = metric
+
+        # Merge results across splits
+        for run, concept, embedding_method, encoder_type in zip(runs, concepts, embedding_methods, encoder_types):
+            results = data[run].dropna()
+
+            if len(results) > 1:
+                raise ValueError(f'Found more than one result for {run}')
+
+            concept_to_embedding_to_encoder_to_results[concept][embedding_method][encoder_type].append(results.iloc[0])
+
+    # Convert defaultdict to regular dict
+    concept_to_embedding_to_encoder_to_results = default_dict_to_regular(concept_to_embedding_to_encoder_to_results)
+
+    # Set up subplots
+    concepts = sorted(concept_to_embedding_to_encoder_to_results)
+    num_plots = len(concepts)
+    fig, axes = plt.subplots(nrows=1, ncols=num_plots, sharey=True, figsize=(4 * num_plots, 4))
+
+    # Plot each set of results
+    for ax, concept in tqdm(zip(axes, concepts), total=num_plots):
+        xticks, xticklabels = [], []
+
+        for embedding_method_idx, embedding_method in enumerate(EMBEDDING_METHODS):
+            for encoder_type_idx, encoder_type in enumerate(ENCODER_TYPES):
+                results = concept_to_embedding_to_encoder_to_results[concept][embedding_method][encoder_type]
+                xtick = encoder_type_idx + embedding_method_idx * (len(ENCODER_TYPES) + OFFSET)
+                xticks.append(xtick)
+                xticklabels.append(encoder_type.upper())
+                ax.bar(
+                    xtick,
+                    np.mean(results),
+                    yerr=np.std(results),
+                    color=ENCODER_TYPE_TO_COLOR[encoder_type],
+                    capsize=5,
+                    hatch=EMBEDDING_METHOD_TO_HATCH[embedding_method]
+                )
+
+        ax.set_xticks(xticks)
+        ax.set_xticklabels(xticklabels)
+        ax.set_xlabel(f'{concept}\n({concept_to_metric[concept]})', weight='bold')
+
+    # Add plot-wide details
+    fig.subplots_adjust(wspace=0)
+    axes[0].set_ylabel('Metric')
+    axes[0].legend(handles=[
+        mpatches.Patch(
+            facecolor='lightgray',
+            hatch=EMBEDDING_METHOD_TO_HATCH[embedding_method],
+            label=EMBEDDING_METHOD_TO_UPPER[embedding_method]
         )
+        for embedding_method in EMBEDDING_METHODS
+    ], loc='upper left')
 
-        for encoder_type_idx, encoder_type in enumerate(encoder_types_order):
-            results = experiment_to_results[f'{embedding_method} {encoder_type}']
-            xtick = encoder_type_idx + embedding_method_idx * (len(encoder_types_order) + OFFSET)
-            xticks.append(xtick)
-            xticklabels.append(encoder_type.upper())
-            ax.bar(
-                xtick,
-                np.mean(results),
-                yerr=np.std(results),
-                color=ENCODER_TYPE_TO_COLOR[encoder_type],
-                capsize=5,
-                hatch=EMBEDDING_METHOD_TO_HATCH[embedding_method]
-            )
-
-    ax.set_xticks(xticks, rotation=45)
-    ax.set_xticklabels(xticklabels, rotation=45)
-    ax.set_ylabel('Metric')
-    ax.set_xlabel(f'{concept} {metric}')
-    ax.legend(handles=legend_items)
+    for ax in axes[1:]:
+        ax.tick_params(axis='y', which='both', length=0)
 
     # Save plot
+    save_path.parent.mkdir(parents=True, exist_ok=True)
     plt.savefig(save_path, bbox_inches='tight')
 
 
