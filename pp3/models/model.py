57bff6aca6c105455d0185c0dae457a0a5f1a81c
Kyle Swanson <swansonk.14@gmail.com>
2023-05-17 20:29:25 -0700

    Skipping roc auc for one class

--- pp3/models/model.py
+++ pp3/models/model.py
@@ -354,6 +354,10 @@ class Model(pl.LightningModule):
 
                     roc_aucs, aps = [], []
                     for i in range(y_arr.shape[1]):
+                        # Skip if there is only one class
+                        if np.unique(y_arr[:, i]) != 2:
+                            continue
+
                         roc_aucs.append(roc_auc_score(y_arr[:, i], y_hat_arr[:, i]))
                         aps.append(average_precision_score(y_arr[:, i], y_hat_arr[:, i]))
 
